67a260a521fda76a81ad28722aa3d2606c168ef5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-07-23 19:55:08 +0530

    Time: 104 ms (31.21%), Space: 75.7 MB (9.5%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(auto i :nums){
+            map[i]++;
+        }
+        for(auto it:map){
+            if(it.second>1) return true;
+        }
+        return false;
+    }
+};
