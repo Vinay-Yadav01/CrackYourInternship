6305f84a185ffeffc60616b60eaa458caaa52157
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-08-09 18:28:20 +0530

    Time: 32 ms (26.62%), Space: 18.1 MB (36.62%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,18 +4,13 @@ public:
         priority_queue<int> pq;
         vector<int> temp;
         for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++)
-                temp.push_back(matrix[i][j]);
-        }
-        int size = temp.size();
-        for (int i = 0; i < k; i++)
-            pq.push(temp[i]);
-
-        for (int i = k; i < size; i++) {
-            if (pq.top() > temp[i]) {
-                pq.pop();
-                pq.push(temp[i]);
+            for (int j = 0; j < matrix[0].size(); j++){
+                pq.push(matrix[i][j]);
+                if (pq.size() > k) {
+                    pq.pop();
+                }
             }
+                
         }
         return pq.top();
     }
