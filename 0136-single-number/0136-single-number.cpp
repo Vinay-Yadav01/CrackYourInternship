53246b11039fbec774e027df0c6096226623ceea
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-08-09 19:11:41 +0530

    Time: 13 ms (64.65%), Space: 19.6 MB (46.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-         map<int, int> m;
-        for (auto i : nums) {
-            m[i]++;
+        int result=0;
+        for(int num : nums){
+            result ^=num;
         }
-        int ans = -1;
-        for (auto it : m) {
-            if (it.second == 1) {
-                ans = it.first;
-                break;
-            }
-        }
-        
-        return ans;
+        return result;
     }
 };
