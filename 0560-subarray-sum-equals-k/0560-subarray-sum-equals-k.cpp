f0f9ef69c2c5a387ff108cde3c48f3e97b2ddbb5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-08-11 18:02:26 +0530

    Time: 59 ms (85.08%), Space: 45.2 MB (62.04%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,10 +4,10 @@ public:
         int ans=0;
         int n = nums.size();
         unordered_map<int,int> mp;
-        mp[0]=1;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
+            if(sum==k) ans++;
             int rem = sum-k;
             if(mp.find(rem)!=mp.end()) ans+=mp[rem];
             mp[sum]++;
