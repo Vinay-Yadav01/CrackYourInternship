cdd7613f45dfe1ab40a5aa774dfaad80e32a69e4
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-08-17 19:13:02 +0530

    Time: 171 ms (44.41%), Space: 90.3 MB (50.14%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,18 +1,16 @@
 class StockSpanner {
 public:
-    vector<int> arr;
-    StockSpanner() {
-        
-    }
-    
+    stack<pair<int, int>> st;
+    int ind;
+    StockSpanner() { ind = -1; }
+
     int next(int price) {
-        arr.push_back(price);
-        int count=0;
-        for(int i=arr.size()-1;i>=0;i--){
-            if(arr[i]<=price) count++;
-            else break;
-        }
-        return count;
+        ind = ind + 1;
+        while (!st.empty() && st.top().first <= price)
+            st.pop();
+        int ans = ind - (st.empty() ? -1 : st.top().second);
+        st.push({price,ind});
+        return ans;
     }
 };
 
