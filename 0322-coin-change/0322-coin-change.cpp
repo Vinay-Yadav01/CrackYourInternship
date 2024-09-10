1ad1617d4bfa62cdd362bf91aeb6ff782a5e5032
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-10 22:03:11 +0530

    Time: 39 ms (51.16%), Space: 20.5 MB (51.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -25,24 +25,47 @@ public:
         //     return -1;
         // return ans;
 
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        // for (int target = 0; target <= amount; target++) {
+        //     if (target % coins[0] == 0)
+        //         dp[0][target] = target / coins[0];
+        //     else
+        //         dp[0][target] = 1e9;
+        // }
+        // for(int ind=1;ind<n;ind++){
+        //     for(int target=0;target<=amount;target++){
+        //         int notTake = dp[ind - 1][target];
+        //         int take = INT_MAX;
+        //         if (coins[ind] <= target)
+        //             take = 1 + dp[ind][target - coins[ind]];
+        //         dp[ind][target] = min(notTake, take);
+        //     }
+        // }
+        // int ans = dp[n - 1][amount];
+        // if (ans == 1e9)
+        //     return -1;
+        // return ans;
+
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        vector<int> prev(amount + 1, 0), curr(amount + 1, 0);
         for (int target = 0; target <= amount; target++) {
             if (target % coins[0] == 0)
-                dp[0][target] = target / coins[0];
+                prev[target] = target / coins[0];
             else
-                dp[0][target] = 1e9;
+                prev[target] = 1e9;
         }
-        for(int ind=1;ind<n;ind++){
-            for(int target=0;target<=amount;target++){
-                int notTake = dp[ind - 1][target];
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 0; target <= amount; target++) {
+                int notTake = prev[target];
                 int take = INT_MAX;
                 if (coins[ind] <= target)
-                    take = 1 + dp[ind][target - coins[ind]];
-                dp[ind][target] = min(notTake, take);
+                    take = 1 + curr[target - coins[ind]];
+                curr[target] = min(notTake, take);
             }
+            prev = curr;
         }
-        int ans = dp[n - 1][amount];
+        int ans = prev[amount];
         if (ans == 1e9)
             return -1;
         return ans;
