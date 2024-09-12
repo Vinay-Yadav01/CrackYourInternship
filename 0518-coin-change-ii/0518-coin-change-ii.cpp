0201c1f58a273d6d1533719acd11f8e73ef38577
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-12 21:29:57 +0530

    Time: 7 ms (71.61%), Space: 20 MB (65.17%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,43 @@
 class Solution {
 private:
-    int solve(vector<int>& coins, int ind, int target,
-              vector<vector<int>>& dp) {
-        if (ind == 0) {
-            if (target % coins[0] == 0)
-                return 1;
-            else
-                return 0;
-        }
-        if (dp[ind][target] != -1)
-            return dp[ind][target];
-        int notTake = solve(coins, ind - 1, target, dp);
-        int take = 0;
-        if (coins[ind] <= target)
-            take = solve(coins, ind, target - coins[ind], dp);
-        return dp[ind][target] = (notTake + take);
-    }
+    // int solve(vector<int>& coins, int ind, int target,
+    //           vector<vector<int>>& dp) {
+    //     if (ind == 0) {
+    //         if (target % coins[0] == 0)
+    //             return 1;
+    //         else
+    //             return 0;
+    //     }
+    //     if (dp[ind][target] != -1)
+    //         return dp[ind][target];
+    //     int notTake = solve(coins, ind - 1, target, dp);
+    //     int take = 0;
+    //     if (coins[ind] <= target)
+    //         take = solve(coins, ind, target - coins[ind], dp);
+    //     return dp[ind][target] = (notTake + take);
+    // }
 
 public:
     int change(int amount, vector<int>& coins) {
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        // return solve(coins, n - 1, amount, dp);
+
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        return solve(coins, n - 1, amount, dp);
-        ;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        for(int target=0;target<=amount;target++){
+            if (target % coins[0] == 0)
+               dp[0][target] = 1;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                int notTake = dp[ind - 1][target];
+                int take = 0;
+                if (coins[ind] <= target)
+                    take = dp[ind][target - coins[ind]];
+                dp[ind][target] = (notTake + take);
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
