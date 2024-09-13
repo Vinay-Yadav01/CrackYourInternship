bfec158b861ac8a21e2e2031ffe18ddd8049549d
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-13 20:35:45 +0530

    Time: 20 ms (74.86%), Space: 8.1 MB (92.65%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -33,17 +33,31 @@ public:
         // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
         // return solve(n,m,text1,text2,dp);
 
+        // int n = text1.size();
+        // int m = text2.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         if (text1[i - 1] == text2[j - 1])
+        //             dp[i][j] = 1 + dp[i - 1][j - 1];
+        //         else
+        //             dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        //     }
+        // }
+        // return dp[n][m];
+
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 else
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                    curr[j] = max(curr[j - 1], prev[j]);
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
