7f22617636f8737f6f7dcd5b8ce0c259a40805b5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-15 22:08:56 +0530

    Time: 762 ms (5.12%), Space: 21.1 MB (6.74%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -8,15 +8,15 @@ public:
         mp[curr] = -1;
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == 'a')
-                state[0] = (state[0] + 1) % 2;
+                state[0] = (state[0] ^ 1);
             else if (s[i] == 'e')
-                state[1] = (state[1] + 1) % 2;
+                state[1] = (state[1] ^ 1);
             else if (s[i] == 'i')
-                state[2] = (state[2] + 1) % 2;
+                state[2] = (state[2] ^ 1);
             else if (s[i] == 'o')
-                state[3] = (state[3] + 1) % 2;
+                state[3] = (state[3] ^ 1);
             else if (s[i] == 'u')
-                state[4] = (state[4] + 1) % 2;
+                state[4] = (state[4] ^ 1);
             curr = "";
             for (int j = 0; j < 5; j++) {
                 curr += to_string(state[j]);
