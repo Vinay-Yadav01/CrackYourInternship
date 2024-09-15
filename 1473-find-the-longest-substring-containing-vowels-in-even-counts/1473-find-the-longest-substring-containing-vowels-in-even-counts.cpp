e0a24a1374158bb224264362469d18c62a0efb3c
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-15 22:15:12 +0530

    Time: 82 ms (40.46%), Space: 18.9 MB (32.79%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     int findTheLongestSubstring(string s) {
         int ans = 0;
-        unordered_map<string, int> mp;
-        vector<int> state(5, 0);
-        string curr = "00000";
-        mp[curr] = -1;
+        unordered_map<int, int> mp;
+        int mask = 0; // Binary representation ....00000
+        mp[mask] = -1;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'a')
-                state[0] = (state[0] ^ 1);
-            else if (s[i] == 'e')
-                state[1] = (state[1] ^ 1);
-            else if (s[i] == 'i')
-                state[2] = (state[2] ^ 1);
-            else if (s[i] == 'o')
-                state[3] = (state[3] ^ 1);
-            else if (s[i] == 'u')
-                state[4] = (state[4] ^ 1);
-            curr = "";
-            for (int j = 0; j < 5; j++) {
-                curr += to_string(state[j]);
+            if (s[i] == 'a') {
+                mask = (mask ^ (1 << 0));
+            } else if (s[i] == 'e') {
+                mask = (mask ^ (1 << 1));
+            } else if (s[i] == 'i') {
+                mask = (mask ^ (1 << 2));
+            } else if (s[i] == 'o') {
+                mask = (mask ^ (1 << 3));
+            } else if (s[i] == 'u') {
+                mask = (mask ^ (1 << 4));
             }
-            if (mp.find(curr) != mp.end()) {
-                ans = max(ans, i - mp[curr]);
-            }
-            else mp[curr] = i;
+
+            if (mp.find(mask) != mp.end()) {
+                ans = max(ans, i - mp[mask]);
+            } else
+                mp[mask] = i;
         }
         return ans;
     }
