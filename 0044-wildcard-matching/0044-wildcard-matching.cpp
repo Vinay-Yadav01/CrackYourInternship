78a68e9d6d61ac0ff1425683cf189cb91fa4ff76
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-16 20:09:20 +0530

    Time: 25 ms (84.44%), Space: 31.9 MB (11.24%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,21 +1,46 @@
 class Solution {
+    // bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+    //     if (i < 0 && j < 0)
+    //         return true;
+    //     if (j < 0)
+    //         return false;
+    //     if (i < 0) {
+    //         for (int ind = 0; ind <= j; ind++) {
+    //             if (p[ind] != '*')
+    //                 return false;
+    //         }
+    //         return true;
+    //     }
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if (s[i] == p[j] || p[j] == '?')
+    //         return dp[i][j] = solve(i - 1, j - 1, s, p, dp);
+    //     else if (p[j] == '*')
+    //         return dp[i][j] =
+    //                    solve(i, j - 1, s, p, dp) || solve(i - 1, j, s, p, dp);
+    //     else
+    //         return dp[i][j] = false;
+    // }
+
+    // 1-based Indexing
+
     bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
-        if (i < 0 && j < 0)
+        if (i == 0 && j == 0)
             return true;
-        if (j < 0)
+        if (j == 0)
             return false;
-        if (i < 0) {
-            for (int ind = 0; ind <= j; ind++) {
-                if (p[ind] != '*')
+        if (i == 0) {
+            for (int ind = 1; ind <= j; ind++) {
+                if (p[ind - 1] != '*')
                     return false;
             }
             return true;
         }
         if (dp[i][j] != -1)
             return dp[i][j];
-        if (s[i] == p[j] || p[j] == '?')
+        if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
             return dp[i][j] = solve(i - 1, j - 1, s, p, dp);
-        else if (p[j] == '*')
+        else if (p[j - 1] == '*')
             return dp[i][j] =
                        solve(i, j - 1, s, p, dp) || solve(i - 1, j, s, p, dp);
         else
@@ -26,7 +51,7 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n - 1, m - 1, s, p, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solve(n, m, s, p, dp);
     }
 };
