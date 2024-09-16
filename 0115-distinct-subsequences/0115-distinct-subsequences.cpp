99631ef39b96efcfdf86cb007a03f6170ffde689
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-16 15:57:30 +0530

    Time: 20 ms (53.81%), Space: 20.1 MB (67.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,12 +1,26 @@
 class Solution {
+    // int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+    //     if (j < 0) // critical Step , first check this base case and then other
+    //         return 1; // t is exhausted
+    //     if (i < 0)
+    //         return 0; // s is exhausted
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if (s[i] == t[j]) {
+    //         return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j, s, t,dp);
+    //     }
+    //     return dp[i][j] = solve(i - 1, j, s, t,dp);
+    // }
+
+    //1-based Indexing
     int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if (j < 0) // critical Step , first check this base case and then other
+        if (j == 0) // critical Step , first check this base case and then other
             return 1; // t is exhausted
-        if (i < 0)
+        if (i == 0)
             return 0; // s is exhausted
         if (dp[i][j] != -1)
             return dp[i][j];
-        if (s[i] == t[j]) {
+        if (s[i-1] == t[j-1]) {
             return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j, s, t,dp);
         }
         return dp[i][j] = solve(i - 1, j, s, t,dp);
@@ -14,9 +28,14 @@ class Solution {
 
 public:
     int numDistinct(string s, string t) {
+        // int n = s.size();
+        // int m = t.size();
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return solve(n - 1, m - 1, s, t, dp);
+
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n - 1, m - 1, s, t, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(n,m, s, t, dp);
     }
 };
