298fef23dfe6f576737d773801b200e9b5d720d6
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-16 16:07:03 +0530

    Time: 15 ms (76.43%), Space: 31.8 MB (22.57%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,30 +1,34 @@
 class Solution {
     // int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-    //     if (j < 0) // critical Step , first check this base case and then other
+    //     if (j < 0) // critical Step , first check this base case and then
+    //     other
     //         return 1; // t is exhausted
     //     if (i < 0)
     //         return 0; // s is exhausted
     //     if (dp[i][j] != -1)
     //         return dp[i][j];
     //     if (s[i] == t[j]) {
-    //         return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j, s, t,dp);
+    //         return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j,
+    //         s, t,dp);
     //     }
     //     return dp[i][j] = solve(i - 1, j, s, t,dp);
     // }
 
-    //1-based Indexing
-    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if (j == 0) // critical Step , first check this base case and then other
-            return 1; // t is exhausted
-        if (i == 0)
-            return 0; // s is exhausted
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        if (s[i-1] == t[j-1]) {
-            return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j, s, t,dp);
-        }
-        return dp[i][j] = solve(i - 1, j, s, t,dp);
-    }
+    // 1-based Indexing
+    //  int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+    //      if (j == 0) // critical Step , first check this base case and then
+    //      other
+    //          return 1; // t is exhausted
+    //      if (i == 0)
+    //          return 0; // s is exhausted
+    //      if (dp[i][j] != -1)
+    //          return dp[i][j];
+    //      if (s[i-1] == t[j-1]) {
+    //          return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j,
+    //          s, t,dp);
+    //      }
+    //      return dp[i][j] = solve(i - 1, j, s, t,dp);
+    //  }
 
 public:
     int numDistinct(string s, string t) {
@@ -33,9 +37,24 @@ public:
         // vector<vector<int>> dp(n, vector<int>(m, -1));
         // return solve(n - 1, m - 1, s, t, dp);
 
+        // int n = s.size();
+        // int m = t.size();
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return solve(n,m, s, t, dp);
+
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return solve(n,m, s, t, dp);
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return (int)dp[n][m];
     }
 };
