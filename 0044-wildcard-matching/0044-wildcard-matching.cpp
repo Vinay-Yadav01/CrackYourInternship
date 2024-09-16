786a068ab6ed483fd214046cf719810b6c691aed
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-16 20:33:08 +0530

    Time: 58 ms (43.07%), Space: 12.2 MB (88.65%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -61,10 +61,36 @@ public:
         // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
         // return solve(n, m, s, p, dp);
 
+        // int n = s.size();
+        // int m = p.size();
+        // vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        // dp[0][0] = true;
+        // for (int j = 1; j <= m; j++) {
+        //     bool allStars = true;
+        //     for (int ind = 1; ind <= j; ind++) {
+        //         if (p[ind - 1] != '*') {
+        //             allStars = false;
+        //             break;
+        //         }
+        //     }
+        //     dp[0][j] = allStars;
+        // }
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+        //             dp[i][j] = dp[i - 1][j - 1];
+        //         else if (p[j - 1] == '*')
+        //             dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+        //         else
+        //             dp[i][j] = false;
+        //     }
+        // }
+        // return dp[n][m];
+
         int n = s.size();
         int m = p.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        dp[0][0] = true;
+        vector<bool> prev(m + 1, false), curr(m + 1, false);
+        prev[0] = true;
         for (int j = 1; j <= m; j++) {
             bool allStars = true;
             for (int ind = 1; ind <= j; ind++) {
@@ -73,18 +99,19 @@ public:
                     break;
                 }
             }
-            dp[0][j] = allStars;
+            prev[j] = allStars;
         }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 else if (p[j - 1] == '*')
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    curr[j] = prev[j] || curr[j - 1];
                 else
-                    dp[i][j] = false;
+                    curr[j] = false;
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
