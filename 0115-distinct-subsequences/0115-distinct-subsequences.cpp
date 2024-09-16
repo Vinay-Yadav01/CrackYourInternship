35409295edf491a2efeb7f4fc97ae75d53a2fac0
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-16 15:55:52 +0530

    Time: 25 ms (33.4%), Space: 20.1 MB (55.04%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if (j < 0) // critical Step , first check this base case and then other
+            return 1; // t is exhausted
+        if (i < 0)
+            return 0; // s is exhausted
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s[i] == t[j]) {
+            return dp[i][j] = solve(i - 1, j - 1, s, t,dp) + solve(i - 1, j, s, t,dp);
+        }
+        return dp[i][j] = solve(i - 1, j, s, t,dp);
+    }
+
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(n - 1, m - 1, s, t, dp);
+    }
+};
