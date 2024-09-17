fcbced5e67ba039ae07b4d9b230f77edfe1f3e42
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-17 21:06:04 +0530

    Time: 5 ms (58.9%), Space: 15.9 MB (43.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,20 +22,37 @@ public:
         // vector<vector<int>> dp(n, vector<int>(2, -1));
         // return solve(0, 1, prices, dp);
 
+        // int n = prices.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        // dp[n][0] = dp[n][1] = 0;
+        // for (int ind = n - 1; ind >= 0; ind--) {
+        //     for (int buy = 0; buy < 2; buy++) {
+        //         if (buy) {
+        //             dp[ind][buy] =
+        //                 max(0 + dp[ind + 1][1], -prices[ind] + dp[ind + 1][0]);
+        //         } else {
+        //             dp[ind][buy] =
+        //                 max(0 + dp[ind + 1][0], prices[ind] + dp[ind + 1][1]);
+        //         }
+        //     }
+        // }
+        // return dp[0][1];
+
         int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-        dp[n][0] = dp[n][1] = 0;
+        vector<int> curr(2, 0), next(2,0);
+        next[0] = next[1] = 0;
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy < 2; buy++) {
                 if (buy) {
-                    dp[ind][buy] =
-                        max(0 + dp[ind + 1][1], -prices[ind] + dp[ind + 1][0]);
+                    curr[buy] =
+                        max(0 + next[1], -prices[ind] + next[0]);
                 } else {
-                    dp[ind][buy] =
-                        max(0 + dp[ind + 1][0], prices[ind] + dp[ind + 1][1]);
+                    curr[buy] =
+                        max(0 + next[0], prices[ind] + next[1]);
                 }
             }
+            next=curr;
         }
-        return dp[0][1];
+        return next[1];
     }
 };
