5900d6d4ba1ca04e4b97804e0bd9654f9aa81e4a
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-25 18:08:40 +0530

    Time: 4 ms (88.3%), Space: 16.2 MB (65.88%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    int solve(int ind, int trans, int k, vector<int>& prices,
-              vector<vector<int>>& dp) {
-        if (ind == prices.size())
-            return 0;
-        if (trans == 2*k)
-            return 0;
-        if (dp[ind][trans] != -1)
-            return dp[ind][trans];
-        if (trans % 2 == 0) {
-            return dp[ind][trans] = max(
-                       -prices[ind] + solve(ind + 1, trans + 1, k, prices, dp),
-                       0 + solve(ind + 1, trans, k, prices, dp));
-        } else {
-            return dp[ind][trans] = max(
-                       prices[ind] + solve(ind + 1, trans + 1, k, prices, dp),
-                       0 + solve(ind + 1, trans, k, prices, dp));
-        }
-    }
+    // int solve(int ind, int trans, int k, vector<int>& prices,
+    //           vector<vector<int>>& dp) {
+    //     if (ind == prices.size())
+    //         return 0;
+    //     if (trans == 2*k)
+    //         return 0;
+    //     if (dp[ind][trans] != -1)
+    //         return dp[ind][trans];
+    //     if (trans % 2 == 0) {
+    //         return dp[ind][trans] = max(
+    //                    -prices[ind] + solve(ind + 1, trans + 1, k, prices,
+    //                    dp), 0 + solve(ind + 1, trans, k, prices, dp));
+    //     } else {
+    //         return dp[ind][trans] = max(
+    //                    prices[ind] + solve(ind + 1, trans + 1, k, prices,
+    //                    dp), 0 + solve(ind + 1, trans, k, prices, dp));
+    //     }
+    // }
 
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -45,9 +45,24 @@ public:
         // Another Approach in which we can reduce Space Complexity and that
         // also applies on Prvious Question too
 
+        // int n = prices.size();
+        // vector<vector<int>> dp(
+        //     n + 1, vector<int>(2*k+1,-1));
+        // return solve(0, 0, k, prices, dp);
+
         int n = prices.size();
-        vector<vector<int>> dp(
-            n + 1, vector<int>(2*k+1,-1));
-        return solve(0, 0, k, prices, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2 * k + 1, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int trans = 0; trans < 2 * k; trans++) {
+                if (trans % 2 == 0) {
+                    dp[ind][trans] = max(-prices[ind] + dp[ind + 1][trans + 1],
+                                         0 + dp[ind + 1][trans]);
+                } else {
+                    dp[ind][trans] = max(prices[ind] + dp[ind + 1][trans + 1],
+                                         0 + dp[ind + 1][trans]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
