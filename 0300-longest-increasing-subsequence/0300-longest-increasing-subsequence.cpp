51aa2cdcc31c5d7fac3daf8051d624f35974bc8c
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-27 19:33:43 +0530

    Time: 242 ms (34.46%), Space: 294.9 MB (22.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -8,25 +8,37 @@ class Solution {
     //     return len;
     // }
 
-    int solve(int ind, int prev_ind, vector<int>& nums,
-              vector<vector<int>>& dp) {
-        if (ind == nums.size())
-            return 0;
-        if (dp[ind][prev_ind+1] != -1)
-            return dp[ind][prev_ind+1];
-        int len = 0 + solve(ind + 1, prev_ind, nums, dp); // Not Take
-        if (prev_ind == -1 || nums[prev_ind] < nums[ind])
-            len = max(len, 1 + solve(ind + 1, ind, nums, dp)); // Take
-        return dp[ind][prev_ind+1] = len;
-    }
+    // int solve(int ind, int prev_ind, vector<int>& nums,
+    //           vector<vector<int>>& dp) {
+    //     if (ind == nums.size())
+    //         return 0;
+    //     if (dp[ind][prev_ind+1] != -1)
+    //         return dp[ind][prev_ind+1];
+    //     int len = 0 + solve(ind + 1, prev_ind, nums, dp); // Not Take
+    //     if (prev_ind == -1 || nums[prev_ind] < nums[ind])
+    //         len = max(len, 1 + solve(ind + 1, ind, nums, dp)); // Take
+    //     return dp[ind][prev_ind+1] = len;
+    // }
 
 public:
     int lengthOfLIS(vector<int>& nums) {
         // return solve(0, -1, nums, dp);
 
         // Co-ordinate Change for prev_ind for memoization
+        // int n = nums.size();
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // return solve(0, -1, nums, dp);
+
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solve(0, -1, nums, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--) {
+                int len = 0 + dp[ind + 1][prev_ind + 1]; // Not Take
+                if (prev_ind == -1 || nums[prev_ind] < nums[ind])
+                    len = max(len, 1 + dp[ind + 1][ind + 1]); // Take
+                dp[ind][prev_ind + 1] = len;
+            }
+        }
+        return dp[0][0];
     }
 };
