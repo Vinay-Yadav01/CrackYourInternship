2607522576e38815263564b6222c82410b5b38b5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-27 19:22:24 +0530

    Time: 516 ms (16.14%), Space: 295 MB (22.46%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    // int solve(int ind, int prev_ind, vector<int>& nums) {
+    //     if (ind == nums.size())
+    //         return 0;
+    //     int len = 0 + solve(ind + 1, prev_ind, nums, dp); // Not Take
+    //     if (prev_ind == -1 || nums[prev_ind] < nums[ind])
+    //         len = max(len, 1 + solve(ind + 1, ind, nums, dp)); // Take
+    //     return len;
+    // }
+
+    int solve(int ind, int prev_ind, vector<int>& nums,
+              vector<vector<int>>& dp) {
+        if (ind == nums.size())
+            return 0;
+        if (dp[ind][prev_ind+1] != -1)
+            return dp[ind][prev_ind+1];
+        int len = 0 + solve(ind + 1, prev_ind, nums, dp); // Not Take
+        if (prev_ind == -1 || nums[prev_ind] < nums[ind])
+            len = max(len, 1 + solve(ind + 1, ind, nums, dp)); // Take
+        return dp[ind][prev_ind+1] = len;
+    }
+
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        // return solve(0, -1, nums, dp);
+
+        // Co-ordinate Change for prev_ind for memoization
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return solve(0, -1, nums, dp);
+    }
+};
