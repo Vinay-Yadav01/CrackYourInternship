b6d871083bda6734ff5c81150ef443787f2cf858
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-09-27 19:38:29 +0530

    Time: 149 ms (43.11%), Space: 13.4 MB (43.22%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -29,16 +29,29 @@ public:
         // vector<vector<int>> dp(n, vector<int>(n+1, -1));
         // return solve(0, -1, nums, dp);
 
+        // int n = nums.size();
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // for (int ind = n - 1; ind >= 0; ind--) {
+        //     for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--) {
+        //         int len = 0 + dp[ind + 1][prev_ind + 1]; // Not Take
+        //         if (prev_ind == -1 || nums[prev_ind] < nums[ind])
+        //             len = max(len, 1 + dp[ind + 1][ind + 1]); // Take
+        //         dp[ind][prev_ind + 1] = len;
+        //     }
+        // }
+        // return dp[0][0];
+
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> curr(n + 1, 0), next(n + 1, 0);
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--) {
-                int len = 0 + dp[ind + 1][prev_ind + 1]; // Not Take
+                int len = 0 + next[prev_ind + 1]; // Not Take
                 if (prev_ind == -1 || nums[prev_ind] < nums[ind])
-                    len = max(len, 1 + dp[ind + 1][ind + 1]); // Take
-                dp[ind][prev_ind + 1] = len;
+                    len = max(len, 1 + next[ind + 1]); // Take
+                curr[prev_ind + 1] = len;
             }
+            next = curr;
         }
-        return dp[0][0];
+        return curr[0];
     }
 };
