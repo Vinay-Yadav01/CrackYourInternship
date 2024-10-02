14fda9b7efeeb3e7dd30fb2f1c221fd8cd2b7d5a
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-10-02 13:50:32 +0530

    Time: 1524 ms (40.9%), Space: 7.8 MB (96.11%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,25 +1,42 @@
 class Solution {
-    bool isPalindrome(int i, int j, string& s){
-        while(i<j){
-            if(s[i++]!=s[j--]) return false;
+    bool isPalindrome(int i, int j, string& s) {
+        while (i < j) {
+            if (s[i++] != s[j--])
+                return false;
         }
         return true;
     }
-    int solve(int i, string &s, vector<int> &dp){
-        if(i==s.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
+    int solve(int i, string& s, vector<int>& dp) {
+        if (i == s.size())
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
         int minCost = INT_MAX;
-        for(int j=i;j<s.size();j++){
-            if(isPalindrome(i,j,s)){
-                minCost = min(minCost, 1+solve(j+1,s,dp));
+        for (int j = i; j < s.size(); j++) {
+            if (isPalindrome(i, j, s)) {
+                minCost = min(minCost, 1 + solve(j + 1, s, dp));
             }
         }
         return dp[i] = minCost;
     }
+
 public:
     int minCut(string s) {
+        // int n = s.size();
+        // vector<int> dp(n,-1);
+        // return solve(0,s,dp)-1;
+
         int n = s.size();
-        vector<int> dp(n,-1);
-        return solve(0,s,dp)-1; 
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int minCost = INT_MAX;
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(i, j, s)) {
+                    minCost = min(minCost, 1 + solve(j + 1, s, dp));
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
     }
 };
