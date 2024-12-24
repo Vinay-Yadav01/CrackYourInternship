55437895a8a01adc8ff09b377a54bbd301f32534
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-12-24 17:51:29 +0530

    Time: 6 ms (81.53%), Space: 42.7 MB (76.18%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,68 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int V, vector<vector<int>>& connections) {
+        DisjointSet ds(V);
+        int cnt=0;
+        for(int i=0;i<connections.size();i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+            if(ds.findUPar(u)==ds.findUPar(v)) cnt++;
+            else ds.unionBySize(u,v);
+        }
+        int connectedComponent = 0;
+        for(int i=0;i<V;i++){
+            if(ds.findUPar(i)==i) connectedComponent++;
+        }
+        return (cnt>=connectedComponent-1)?connectedComponent-1:-1;
+    }
+};
