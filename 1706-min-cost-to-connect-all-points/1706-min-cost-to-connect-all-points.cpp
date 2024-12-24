9a7a2e67fdc9ab568f76d4ef434f440d7e142936
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-12-24 13:42:01 +0530

    Time: 367 ms (52.8%), Space: 46.8 MB (67.68%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -2,34 +2,6 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<pair<int,int>> adj[n];
-        int u=0;
-        for(auto it1: points){
-            int v = 0;
-            for(auto it2: points){
-                if(it1==it2) {
-                    v++;
-                    continue;
-                }
-                int x1 = it1[0], y1 = it1[1];
-                int x2 = it2[0], y2 = it2[1];
-                int dist = abs(x2-x1) + abs(y2-y1);
-                adj[u].push_back({v,dist});
-                // cout<<u<<" "<<v<<endl;
-                v++;
-            }
-            u++;
-        }
-
-        // cout<<endl<<endl;
-
-        // for(int it=0;it<n;it++){
-        //     for(auto i: adj[it]){
-        //         cout<<it<<" "<<i.first<<" "<<i.second<<endl;
-        //     }
-        // }
-
-        //Graph is ready now
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         pq.push({0,0}); //wt node
         int sum = 0;
@@ -42,10 +14,10 @@ public:
             if(vis[node]==1) continue;
             vis[node]=1;
             sum+=wt;
-            for(auto it: adj[node]){
-                int adjNode = it.first;
-                int wt = it.second;
-                if(!vis[adjNode]) pq.push({wt,adjNode});
+            for(int adjNode=0;adjNode<n;adjNode++){
+                if(vis[adjNode]) continue;
+                int wt = abs(points[node][0]-points[adjNode][0]) + abs(points[node][1]-points[adjNode][1]);
+                pq.push({wt, adjNode});
             }
         }
         return sum;
