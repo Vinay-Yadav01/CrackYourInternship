6dc5a9510bdab44710fb93c96a4ed35afc7a3f45
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-03-10 16:21:59 +0530

    Time: 3 ms (81.82%), Space: 10.6 MB (45.76%) - LeetHub

---
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    bool isVowel(char& ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        else
+            return false;
+    }
+
+    int countAtLeastK(string word, int k) {
+        int n = word.size();
+        unordered_map<char, int> vowels;
+        int consonants = 0;
+        int l = 0, r = 0;
+        int count = 0;
+        while (r < n) {
+            char ch = word[r];
+            if (isVowel(ch)) {
+                vowels[ch]++;
+            } else {
+                consonants++;
+            }
+            while (vowels.size() == 5 && consonants >= k) {
+                count += (n - r);
+                char temp = word[l];
+                if (isVowel(temp)) {
+                    vowels[temp]--;
+                    if (vowels[temp] == 0)
+                        vowels.erase(temp);
+                } 
+                else consonants--;
+                l++;
+            }
+            r++;
+        }
+        return count;
+    }
+
+public:
+    int countOfSubstrings(string word, int k) {
+        return countAtLeastK(word, k) - countAtLeastK(word, k + 1);
+    }
+};
