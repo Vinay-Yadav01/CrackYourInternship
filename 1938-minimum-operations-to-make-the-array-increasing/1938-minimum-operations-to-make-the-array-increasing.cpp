c4379adc1456f2e54bf2e3bfae738fd9c835a093
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-03-12 20:19:31 +0530

    Time: 12 ms (41.9%), Space: 19.4 MB (84%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int operations = 0;
-        int n = nums.size();
-        for (int i = 1; i < n; i++) {
-            while (nums[i] <= nums[i - 1]) {
-                nums[i]++;
-                operations++;
+        int ans = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                int diff = abs(nums[i - 1] - nums[i]);
+                nums[i] = nums[i - 1] + 1;
+                ans += diff + 1;
             }
         }
-        return operations;
+        return ans;
     }
 };
