18788ad289fb858159c87418d2d693b66c319497
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-04-10 15:22:54 +0530

    Time: 44 ms (26.4%), Space: 104.8 MB (38.4%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -8,10 +8,12 @@ public:
         }
         int champTeam = -1;
         for (int i = 0; i < n; i++) {
-            if (champTeam != -1 && indegree[i] == 0)
-                return -1;
-            if (indegree[i] == 0)
+            if (indegree[i] == 0) {
+                if (champTeam != -1) {
+                    return -1; // More than one team with in-degree 0
+                }
                 champTeam = i;
+            }
         }
         return champTeam;
     }
