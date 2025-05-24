b1e3635729c7d337454079161cb3425399eb9d5f
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-05-24 22:15:01 +0530

    Time: 1476 ms (8.94%), Space: 50.4 MB (79.32%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    vector<int> nums;
+
+public:
+    Solution(vector<int>& nums) { this->nums = nums; }
+
+    int pick(int target) {
+        int count = 0;
+        int result = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                count++;
+                if (rand() % count == 0) {
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+};
