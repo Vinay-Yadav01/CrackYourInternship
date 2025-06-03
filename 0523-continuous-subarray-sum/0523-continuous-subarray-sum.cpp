fac23f505828aa9bbb204dc3ffcc4febb5111569
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-06-03 11:52:28 +0530

    Time: 80 ms (38.05%), Space: 146.4 MB (42.44%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefixSum(n);
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-        }
-        int i = 0;
+        int i = 0, sum = 0;
         unordered_map<int, int> mp;
         mp[0] = 0;
         while (i < n) {
-            if (i > 0 && prefixSum[i] % k == 0)
+            sum += nums[i];
+            if (i > 0 && sum % k == 0)
                 return true;
-            int rem = prefixSum[i] % k;
+            int rem = sum % k;
             if (mp.find(rem) != mp.end()) {
                 int ind = mp[rem];
                 if (i - ind > 1)
                     return true;
-            } else if (mp.find(prefixSum[i]) != mp.end()) {
-                int ind = mp[prefixSum[i]];
+            } else if (mp.find(sum) != mp.end()) {
+                int ind = mp[sum];
                 if (i - ind > 1)
                     return true;
             } else
