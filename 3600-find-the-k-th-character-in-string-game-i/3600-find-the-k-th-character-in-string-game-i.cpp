ce36caef8ec068da2d4aaa3b640e85dac140e8e7
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-07-03 10:57:18 +0530

    Time: 4 ms (36.05%), Space: 10.4 MB (33.67%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,26 +1,20 @@
 class Solution {
+    string f(string str) {
+        string ans = "";
+        for (char ch : str) {
+            ans += (ch - 'a' + 1) % 26 + 'a';
+        }
+        return ans;
+    }
+
 public:
     char kthCharacter(int k) {
-        string word = "a";  // Start with initial string "a"
-        
-        // Continue appending transformed string until word's length is at least k
-        while (word.size() < k) {
-            string nextPart = "";  // The part that will be appended to word
-            
-            // Generate the next characters by shifting each character in word by 1
-            for (char ch : word) {
-                if (ch == 'z') {
-                    nextPart += 'a';  // 'z' rolls over to 'a'
-                } else {
-                    nextPart += (ch + 1);  // Other characters are incremented
-                }
-            }
-            
-            // Append the new part to word
-            word += nextPart;
+        string str = "a";
+        while (str.size() <= k) {
+            string next_str = f(str);
+            str += next_str;
         }
-        
-        // Return the k-th character (1-indexed, so we use k-1 for 0-indexed string)
-        return word[k - 1];
+        cout<<str;
+        return str[k-1];
     }
 };
