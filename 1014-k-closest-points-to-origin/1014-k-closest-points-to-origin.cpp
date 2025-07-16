d06d1d390a74350412b1abe3d93802400879d297
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-07-16 19:29:24 +0530

    Time: 71 ms (45.97%), Space: 74.3 MB (52.58%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -7,13 +7,9 @@ public:
         for (int i = 0; i < points.size(); i++) {
             int dist =
                 points[i][0] * points[i][0] + points[i][1] * points[i][1];
-            if (pq.size() < k) {
-                pq.push({dist, {points[i][0], points[i][1]}});
-            } else {
-                if (pq.top().first > dist) {
-                    pq.pop();
-                    pq.push({dist, {points[i][0], points[i][1]}});
-                }
+            pq.push({dist, {points[i][0], points[i][1]}});
+            if (pq.size() > k) {
+                pq.pop();
             }
         }
         while (!pq.empty()) {
