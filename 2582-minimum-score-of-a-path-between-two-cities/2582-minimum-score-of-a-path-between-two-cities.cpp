ba69d139c308546e2d48a71a6c1ba33f5685d70a
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-07-18 19:52:31 +0530

    Time: 64 ms (64.92%), Space: 132.6 MB (76.76%) - LeetHub

--- 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
         vector<vector<pair<int, int>>> adj(n + 1);
-        for (auto road : roads) {
-            int u = road[0], v = road[1], dist = road[2];
-            adj[u].push_back({v, dist});
-            adj[v].push_back({u, dist});
+        for (auto& road : roads) {
+            int u = road[0], v = road[1], w = road[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
         }
-        vector<int> dist(n + 1, INT_MAX);
+
+        vector<bool> visited(n + 1, false);
         queue<int> q;
         q.push(1);
+        visited[1] = true;
+
+        int minEdge = INT_MAX;
         while (!q.empty()) {
             int node = q.front();
             q.pop();
-            for (auto nbr : adj[node]) {
-                int nbrCity = nbr.first, distance = nbr.second;
-                if (dist[nbrCity] == INT_MAX ||
-                    min(dist[node], distance) < dist[nbrCity]) {
-                    dist[nbrCity] = min(dist[node], distance);
-                    q.push(nbrCity);
+            for (auto& [nei, w] : adj[node]) {
+                minEdge = min(minEdge, w);
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    q.push(nei);
                 }
             }
         }
-        return dist[n];
+
+        return minEdge;
     }
 };
