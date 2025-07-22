686c6985c485084e42fe9036439a66b42b4f3347
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-07-22 19:58:48 +0530

    Time: 15 ms (35.97%), Space: 26.7 MB (34.9%) - LeetHub

---
+++ 1489-pizza-with-3n-slices/1489-pizza-with-3n-slices.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int start, int end, vector<int>& slices, int n,
+              vector<vector<int>>& dp) {
+        if (n == 0 || start > end)
+            return 0;
+
+        if (dp[start][n] != -1)
+            return dp[start][n];
+        int incl = slices[start] + solve(start + 2, end, slices, n - 1, dp);
+        int excl = 0 + solve(start + 1, end, slices, n, dp);
+
+        return dp[start][n] = max(incl, excl);
+    }
+    int maxSizeSlices(vector<int>& slices) {
+        int k = slices.size();
+        vector<vector<int>> dp1(k, vector<int>(k, -1));
+        int case1 = solve(0, k - 2, slices, k / 3, dp1);
+        vector<vector<int>> dp2(k, vector<int>(k, -1));
+        int case2 = solve(1, k - 1, slices, k / 3, dp2);
+        return max(case1, case2);
+    }
+};
