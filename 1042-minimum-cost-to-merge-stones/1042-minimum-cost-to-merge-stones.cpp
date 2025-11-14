777e3382e2b9bb31560f17d2ed29ec6e50499da0
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-11-14 22:04:34 +0530

    Time: 2 ms (56.5%), Space: 11.2 MB (56.18%) - LeetHub

---
+++ 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solve(vector<int>& stones, int i, int j, int k, vector<int>& pre,
+              vector<vector<int>>& dp) {
+        if (i >= j)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int ans = INT_MAX;
+        for (int index = i; index < j; index = index + k - 1) {
+            int tmp = solve(stones, i, index, k, pre, dp) +
+                      solve(stones, index + 1, j, k, pre, dp);
+            ans = min(ans, tmp);
+        }
+
+        if ((j - i) % (k - 1) == 0) {
+            ans = ans + pre[j + 1] - pre[i];
+        }
+        return dp[i][j] = ans;
+    }
+    int mergeStones(vector<int>& stones, int k) {
+        int n = stones.size();
+        if ((n - 1) % (k - 1) != 0) {
+            return -1;
+        }
+
+        vector<int> pre(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            pre[i] = pre[i - 1] + stones[i - 1];
+        }
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return solve(stones, 0, n - 1, k, pre, dp);
+    }
+};
