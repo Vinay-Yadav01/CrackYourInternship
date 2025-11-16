4cc68d546a033131c2e8777965165698518d3c64
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-11-16 22:46:48 +0530

    Time: 4 ms (46.51%), Space: 26.3 MB (49.98%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+        for(int num: nums){
+            freq[num]++;
+        }
+        vector<int> ans(2);
+        for(int i = 1; i<freq.size();i++){
+            if(freq[i] == 2) ans[0] = i;
+            if(freq[i] == 0) ans[1] = i;
+        }
+        return ans;
+    }
+};
