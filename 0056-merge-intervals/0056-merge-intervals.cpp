6f08ebc5c4343ac3807af4b825b8d05b6c425ec2
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2025-11-19 17:08:12 +0530

    Time: 6 ms (48.42%), Space: 23.9 MB (59.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,21 +4,11 @@ public:
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        int prevStart = intervals[0][0];
-        int prevEnd = intervals[0][1];
-        int i = 0;
-        while (i < n) {
-            int currStart = intervals[i][0];
-            while (i < n && currStart <= prevEnd) {
-                prevEnd = max(prevEnd, intervals[i][1]);
-                i++;
-                if (i < n)
-                    currStart = intervals[i][0];
-            }
-            ans.push_back({prevStart, prevEnd});
-            if (i < n) {
-                prevStart = intervals[i][0];
-                prevEnd = intervals[i][1];
+        for (int i = 0; i < n; i++) {
+            if (ans.empty() || ans.back()[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
+            } else {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
         }
         return ans;
